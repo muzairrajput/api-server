6981e0a8a7c9f98f722e7676f66f06f4978cf5b2
HussnatHaider <hussnatravian@gmail.com>
2023-11-13 04:29:47 -0500

    Post API for message

--- server.js
+++ server.js
@@ -41,18 +41,7 @@ app.post('/merchant', (req, res) => {
     });
 });
 
-app.get('/message', (req, res) => {
-    // Use a SELECT query to retrieve data from the User table
-    dbConnection.query('SELECT * FROM Message', (error, results) => {
-        if (error) {
-            console.error('Error retrieving data: ' + error.message);
-            res.status(500).send('Error retrieving data from the database');
-        } else {
-            console.log('Data retrieved from Message table');
-            res.status(200).json(results); // Send the retrieved data as a JSON response
-        }
-    });
-});
+
 
 app.get('/user', (req, res) => {
     console.log('GET request received at /user route'); // Log to verify that the route handler is reached
@@ -261,6 +250,37 @@ app.post('/Customer', (req, res) => {
     });
 });
 
+app.get('/message', (req, res) => {
+    // Use a SELECT query to retrieve data from the User table
+    dbConnection.query('SELECT * FROM Message', (error, results) => {
+        if (error) {
+            console.error('Error retrieving data: ' + error.message);
+            res.status(500).send('Error retrieving data from the database');
+        } else {
+            console.log('Data retrieved from Message table');
+            res.status(200).json(results); // Send the retrieved data as a JSON response
+        }
+    });
+});
+
+app.post('/message', (req, res) => {
+    console.log('POST request received at create Message route'); // Log to verify that the route handler is reached
+    // Use the data from the request body
+    const {chatroomId, senderId, content, timesent} = req.body;
+    const msgData = req.body;
+    console.log('Data to be inserted:', msgData); // Log to check the data being used for the insertion
+    var query = `Insert into Message (ChatRoom_ID, Sender_ID, Content, TimeSent) 
+    values (${chatroomId}, ${senderId},'${content}', ${timesent})`;
+    dbConnection.query(query, msgData, (error, results) => {
+        if (error) {
+            console.error('Error inserting data: ' + error.message);
+            res.status(500).send('Error inserting data into the database');
+        } else {
+            console.log('Data inserted into Message table');
+            res.status(200).send('Record added to the Message table');
+        }
+    });
+});
 
 
 app.listen(8083);
