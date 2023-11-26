0b0c0e5ac994dc8b231726987c23349a74561fc8
muhammad uzair <uzair@plutus21.com>
2023-11-26 03:02:52 -0500

    Product Controller added having APIs Search all products with two optional Parameters (category, vendorId) Add Product Get Product By Id

---
+++ productController.js
@@ -0,0 +1,68 @@
+const router = require("express").Router();
+var dbConnection = require('./dbCon');
+
+router.get('/', (req, res) => {
+    // Use a SELECT query to retrieve data from the User table
+    const {category, vendorId} = req.query;
+    console.log(category);
+    let sql = "SELECT * FROM Product WHERE 1=1";
+    if (category) {
+        sql += ` AND Category = '${category}'`;
+    }
+    if (vendorId) {
+        sql += ` AND Vendor_ID = ${vendorId}`;
+    }
+    console.log('SQL: '+sql);
+    dbConnection.query(sql, (error, results) => {
+        if (error) {
+            console.error('Error retrieving data: ' + error.message);
+            res.status(500).send('Error retrieving data from the database');
+        } else {
+            console.log('Data retrieved from Product table');
+            res.status(200).json(results); // Send the retrieved data as a JSON response
+        }
+    });
+});
+
+//retrieving all messages by chatroom id
+router.get('/:id', (req, res) => {
+    console.log('GET request received at /order route'); // Log to verify that the route handler is reached
+
+    // Extract the orderId parameter from the URL
+    const Product_ID = req.params.id;
+
+    // Query string using the orderId parameter
+    const sql = "SELECT * FROM Product WHERE Product_ID = ?";
+
+    dbConnection.query(sql, [Product_ID], (error, results) => {
+        if (error) {
+            console.error('Error retrieving data: ' + error.message);
+            res.status(500).send('Error retrieving data from the database');
+        } else {
+            console.log('Data retrieved from Product table');
+            res.status(200).json(results); // Send the retrieved data as a JSON response
+        }
+    });
+});
+
+router.post('/', (req, res) => {
+    console.log('POST request received at create Product route'); // Log to verify that the route handler is reached
+    // Use the data from the request body
+    const {name, description, category, price, stock, vendorId} = req.body;
+    const msgData = req.body;
+    console.log('Data to be inserted:', msgData); // Log to check the data being used for the insertion
+    var query = `INSERT INTO Product (Name, Description, Category, Price, StockQuantity, Vendor_ID)
+    values ('${name}', '${description}','${category}', ${price}, ${stock}, ${vendorId})`;
+    console.log(query);
+    dbConnection.query(query, msgData, (error, results) => {
+        if (error) {
+            console.error('Error inserting data: ' + error.message);
+            res.status(500).send('Error inserting data into the database');
+        } else {
+            console.log('Data inserted into Product table');
+            res.status(200).send('Record added to the Product table');
+        }
+    });
+});
+
+module.exports = router;
--- routes.js
+++ routes.js
@@ -4,11 +4,13 @@ var message = require('./messageController');
 var customer = require('./customerController');
 var chatroom = require('./chatRoomController');
 var order = require('./orderController');
+var product = require('./productController');
 
 router.use('/merchant', merchant);
 router.use('/message', message);
 router.use('/customer', customer);
 router.use('/chatroom', chatroom);
 router.use('/order', order);
+router.use('/product', product);
 
 module.exports  = router;
