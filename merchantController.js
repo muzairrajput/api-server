6ffb36f4f7f9d3f4d2f06094a1a9f9d0f19a789f
HussnatHaider <hussnatravian@gmail.com>
2023-11-30 20:40:31 -0500

    Merchant signup API created

--- merchantController.js
+++ merchantController.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 var dbConnection = require('./dbCon');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
     console.log('GET request received at merchant get route');
@@ -32,6 +33,60 @@ router.post('/', (req, res) => {
             console.log('Data inserted into Merchant table');
             res.status(200).send('Record added to the Merchant table');
         }
+    });
+});
+
+// Define a route to handle POST requests for data insertion
+router.post('/signup', (req, res) => {
+    console.log('POST request received at /signup route');
+
+    const {Username, Password, Email, Address, Phone, BusinessName, BusinessDescription, BusinessLicenseNumber} = req.body;
+
+    if (!Username || !Password || !Email || !Address || !Phone || !BusinessName || !BusinessDescription || !BusinessLicenseNumber) {
+        return res.status(400).json({ error: 'All fields must be filled for successful signup.' });
+    }
+
+    // Hash the password
+    const saltRounds = 10; // You can adjust the number of salt rounds as needed
+    const hashedPassword = bcrypt.hashSync(Password, saltRounds);
+
+
+    const query = "SELECT * FROM User WHERE Username = ?";
+    const userData = [Username];
+
+    dbConnection.query(query, userData, (error, result) => {
+        if (error) {
+            console.error('Error querying the database: ' + error.message);
+            return res.status(500).json({ error: 'Error querying the database' });
+        }
+
+        if (result.length > 0) {
+            // A matching user already exists
+            return res.status(400).json({ error: 'User with the same username already exists' });
+        }
+        const insertedQuery = "INSERT INTO User (Username, Password, Email, Address, Phone) VALUES (?, ?, ?, ?, ?)";
+        const insertedUserData = [Username, hashedPassword, Email, Address, Phone];
+
+        dbConnection.query(insertedQuery, insertedUserData, (error, results) => {
+            if (error) {
+                console.error('Error inserting data: ' + error.message);
+                res.status(500).send('Error inserting data into the database');
+            } else {
+                console.log('Data inserted into User table');
+                console.log(results);
+                const insertQuery = "INSERT INTO Merchant (Email, Address, Phone, BusinessName, BusinessDescription, BusinessLicenseNumber, User_ID) VALUES (?, ?, ?, ?, ?, ?, ?)";
+                const insertData = [Email, Address, Phone, BusinessName, BusinessDescription, BusinessLicenseNumber, results.insertId];
+                dbConnection.query(insertQuery, insertData, (error, results) => {
+                    if (error) {
+                        console.error('Error inserting merchant: ' + error.message);
+                        res.status(500).send('Error inserting merchant into the database');
+                    } else {
+                        res.status(200).send('Record added to the User table');
+                    }
+                });
+            }
+        });
+       
     });
 });
 
